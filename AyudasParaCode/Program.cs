b3fb7d872d8e9d08b324c5569d3c684c81c58949
JeanMMP1408 <jeanmanuelmartinez45@gmail.com>
2023-12-16 22:13:15 -0500

    Funcion de Pila

---
+++ AyudasParaCode/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace AyudaParaCode
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ClaseAyudas F = new ClaseAyudas();
+            string s= Console.ReadLine();
+            Queue<string> queue = F.GetQueue(s);
+            while (queue.Count > 0)
+            Console.WriteLine(queue.Dequeue());
+        }
+    }
+}
